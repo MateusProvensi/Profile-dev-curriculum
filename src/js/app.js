c96eb3d6ff381cf694000e10e3f342fdedb9517f
MateusProvensi <mateussprovensidev@outlook.com>
2021-09-07 00:02:26 -0300

    Adicionado HTML, CSS e JS principal

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+const navCabecalho = Array.from(
+  document.querySelectorAll(".botoes-cabecalho > *")
+);
+
+navCabecalho.forEach((elemento) => {
+  const conteudoPadrao = elemento.innerHTML;
+
+  elemento.onmouseover = (e) => {
+    e.preventDefault();
+
+    elemento.innerHTML = `< ${conteudoPadrao} >`;
+    elemento.classList.add("verde-principal-color");
+  };
+
+  elemento.onmouseout = (e) => {
+    e.preventDefault();
+
+    elemento.innerHTML = `${conteudoPadrao}`;
+    elemento.classList.remove("verde-principal-color");
+  };
+
+  elemento.onclick = (e) => {
+    e.preventDefault();
+
+    const destinoOriginal = document.querySelectorAll(
+      elemento.getAttribute("href")
+    )[0];
+
+    if (destinoOriginal.id === "experiencias-sobremim") {
+      window.scrollTo(0, destinoOriginal.scrollHeight - -50);
+    } else if (destinoOriginal.id === "tecnologias") {
+      window.scrollTo(0, destinoOriginal.scrollHeight - 35);
+    } else if (destinoOriginal.id === "contatos") {
+      window.scrollTo(0, destinoOriginal.scrollHeight - -900);
+    }
+  };
+});
